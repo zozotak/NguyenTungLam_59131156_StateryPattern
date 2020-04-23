db11e0608a22dd69f6058f676fa133bb180ff6ca
DELL <zozotak.boss@gmail.com>
2020-04-23 19:24:04 +0700

    v 1.1

--- src/Bai1/Bai1main.java
+++ src/Bai1/Bai1main.java
@@ -15,25 +15,13 @@ public class Bai1main {
      * @param args the command line arguments
      */
    public static void main(String[] args) {
-        context pheptinh1 = new context();
-        ITinh cong = new Cong();
-        pheptinh1.setTinhToan(cong);
-        System.out.println("Kết quả phép tính cộng: " + pheptinh1.tinh(1,2));
-        
-        context pheptinh2 = new context();
-        ITinh tru = new Tru();
-        pheptinh2.setTinhToan(tru);
-        System.out.println("Kết quả phép tính trừ: " + pheptinh2.tinh(3, 4));
-        
-        context pheptinh3 = new context();
-        ITinh nhan = new Nhan();
-        pheptinh3.setTinhToan(nhan);
-        System.out.println("Kết quả phép tính nhân: " + pheptinh3.tinh(5, 6));
-        
-        context pheptinh4 = new context();
-        ITinh chia = new Chia();
-        pheptinh4.setTinhToan(chia);
-        System.out.println("Kết quả phép tính chia: " + pheptinh4.tinh(7, 8));
+        // TODO code application logic here
+        context ct = new context();
+        ct.setTinhToan(new Cong());
+        System.out.println("75+12= "+ct.tinh(75, 12));
+        context ct2 = new context();
+        ct2.setTinhToan(new Tru());
+        System.out.println("54-78= "+ct2.tinh(54, 78));
     }
     
 }
